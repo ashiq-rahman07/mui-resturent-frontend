86b0501afdf872caf0b2ca063e8018416206d06e
Ashiq <web3.0.ashiq@gmail.com>
2023-04-25 19:00:15 +0600

    header section add

--- src/components/Layaout/Header.jsx
+++ src/components/Layaout/Header.jsx
@@ -1,8 +1,92 @@
-
-
+import React, { useState } from 'react'
+import {AppBar, Box, Drawer, IconButton, Toolbar, Typography} from '@mui/material'
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
+import '../../styles/HeaderStyles.css';
 const Header = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  //handle menu click 
+  const handleDrawerToggle = () => {
+      setMobileOpen(!mobileOpen)
+  }
+  //menu drawer
+  const drawer = ( 
+    <Box onClick={handleDrawerToggle} sx={{textAlign:'center'}}>
+        <Typography component='div' variant='h6' color={"goldenrod"} sx={{flexGrow: 1, my:2}} >
+              <FastfoodIcon/>
+                My Resturent
+            </Typography>
+          
+              <ul className="mobile-navigation">
+                <li>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li>
+                  <Link to='/menu'>Menu</Link>
+                </li>
+                <li>
+                  <Link to='/about'>About</Link>
+                </li>
+                <li>
+                  <Link to='/contact'>Contact</Link>
+                </li>
+              </ul>
+           
+    </Box>
+  )
   return (
-    <div>Header</div>
+    <>
+      <Box>
+        <AppBar component={"nav"} sx={{bgcolor:"black"}}>
+          <Toolbar>
+            <IconButton 
+            color='inherit' 
+            aria-level="open drawer" 
+            edge="start"
+            sx={{mr: 2, display:{sm: "none"}}}
+            onClick={handleDrawerToggle}
+            >
+                <MenuIcon/>
+            </IconButton>
+            <Typography component='div' variant='h6' color={"goldenrod"} sx={{flexGrow: 1}} >
+              <FastfoodIcon/>
+                My Resturent
+            </Typography>
+            <Box sx={{display:{xs: 'none', sm:'block'}}}>
+              <ul className='navigation-menu'>
+                <li>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li>
+                  <Link to='/menu'>Menu</Link>
+                </li>
+                <li>
+                  <Link to='/about'>About</Link>
+                </li>
+                <li>
+                  <Link to='/contact'>Contact</Link>
+                </li>
+              </ul>
+            </Box>
+          </Toolbar>
+          
+        </AppBar>
+        <Box>
+          <Drawer 
+          component="nav" 
+          open={mobileOpen} 
+          onClose={handleDrawerToggle}
+          sx={{display:{xs: "block", sm:"none"},"& .MuiDrawer-paper":{
+            boxSizing: 'border-box',
+            width:"240px"
+          }}}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+      </Box>
+    </>
   )
 }
 
